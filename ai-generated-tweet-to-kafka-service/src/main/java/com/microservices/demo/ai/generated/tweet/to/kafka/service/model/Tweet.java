0cb3047537d18b205679b27dcd5269f70bd76c27
kartik9555 <kartik9555@gmail.com>
2026-02-05 21:57:54 +0100

    Updated date format

--- ai-generated-tweet-to-kafka-service/src/main/java/com/microservices/demo/ai/generated/tweet/to/kafka/service/model/Tweet.java
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/microservices/demo/ai/generated/tweet/to/kafka/service/model/Tweet.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.ZonedDateTime;
 
 public record Tweet(
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "EEE MMM dd HH:mm:ss zzz yyy")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "EEE MMM dd HH:mm:ss zzz yyyy")
         ZonedDateTime createdAt,
         Long id,
         String text,
