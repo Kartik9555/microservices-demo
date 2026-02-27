b8eb9d43a8080af2ba561dadd4dc6d386ef65e91
kartik9555 <kartik9555@gmail.com>
2026-02-27 13:57:17 +0100

    Implemented Api Gateway with Spring cloud gateway, Redis and Resilience4J

---
+++ gateway-service/src/main/java/com/microservices/demo/gateway/service/confg/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.microservices.demo.gateway.service.confg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        http.authorizeExchange(exchange -> exchange
+                .anyExchange().permitAll());
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+}
